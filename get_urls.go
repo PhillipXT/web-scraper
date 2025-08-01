4a1677eb8362aa9f7711f4f4d6c18d4cc9c387a9
Phillip Theriault <phillip.theriault@spellweaver.com>
2025-08-01 12:29:12 -0600

    Retrieve list of urls from html string

---
+++ get_urls.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+
+	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
+)
+
+// Added a package to make this work:  go get golang.org/x/net/html
+func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
+
+	baseURL, err := url.Parse(rawBaseURL)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't parse base URL: %v", err)
+	}
+
+	var links []string
+
+	doc, err := html.Parse((strings.NewReader(htmlBody)))
+	if err != nil {
+		return nil, fmt.Errorf("couldn't parse HTML: %v", err)
+	}
+
+	for node := range doc.Descendants() {
+		if node.Type == html.ElementNode && node.DataAtom == atom.A {
+			for _, a := range node.Attr {
+				if a.Key == "href" {
+					href, err := url.Parse(a.Val)
+					if err != nil {
+						return nil, fmt.Errorf("couldn't parse href '%v': %v", a.Val, err)
+					}
+					resolvedURL := baseURL.ResolveReference(href)
+					links = append(links, resolvedURL.String())
+				}
+			}
+		}
+	}
+
+	return links, nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/PhillipXT/web-scraper
 
-go 1.23.4
+go 1.24.5
+
+require golang.org/x/net v0.42.0
